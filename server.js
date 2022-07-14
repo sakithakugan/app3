e87566b7a89834472c61e6deae9b3497b4bfe584
sakitha <kukansakitha@gmail.com>
2022-07-14 08:12:00 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.use(bodyParser.json())
+
+const dbConfig = require('./config/database.config.js');
+
+const UserRoute = require('./app/routes/User')
+
+app.use('/user',UserRoute)
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Databse Connected Successfully!!");    
+}).catch(err => {
+    console.log('Could not connect to the database', err);
+    process.exit();
+});
+
+app.get('/', (req, res) => {
+    res.json({"message": "Hello Crud Node Express"});
+});
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
